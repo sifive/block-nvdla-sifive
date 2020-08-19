fdf14de097fb01c1df0866fdaee4e388bf0f8021
Erik Danie <erik.danie@sifive.com>
2020-08-19 14:31:28 -0700

    change control_bus to cbus

--- src/main/scala/devices/nvdla/Periphery.scala
+++ src/main/scala/devices/nvdla/Periphery.scala
@@ -18,7 +18,7 @@ trait HasPeripheryNVDLA { this: BaseSubsystem =>
       TLBuffer.chainNode(p(NVDLAFrontBusExtraBuffers))
     } := nvdla.dbb_tl_node
 
-    sbus.control_bus.toFixedWidthSingleBeatSlave(4, Some("nvdla_cfg")) { nvdla.cfg_tl_node }
+    cbus.toFixedWidthSingleBeatSlave(4, Some("nvdla_cfg")) { nvdla.cfg_tl_node }
 
     ibus.fromSync := nvdla.int_node
   }
